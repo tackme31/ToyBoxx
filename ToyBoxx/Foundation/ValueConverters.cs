2ddca91c5635c12f4c4776535ba981abb2c066e5
Takumi Yamada <xirtardauq@gmail.com>
2025-09-13 20:50:24 +0900

    Add position slider

--- ToyBoxx/Foundation/ValueConverters.cs
+++ ToyBoxx/Foundation/ValueConverters.cs
@@ -1,11 +1,12 @@
 ﻿using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace ToyBoxx.Foundation;
 
 /// <inheritdoc />
 [ValueConversion(typeof(bool), typeof(bool))]
-public class InverseBooleanConverter : IValueConverter
+internal class InverseBooleanConverter : IValueConverter
 {
     /// <inheritdoc />
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -49,5 +50,40 @@ public class InverseBooleanConverter : IValueConverter
         }
 
         return true;
+    }
+}
+
+internal class TimeSpanToSecondsConverter : IValueConverter
+{
+    /// <inheritdoc />
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            TimeSpan span => span.TotalSeconds,
+            Duration duration => duration.HasTimeSpan ? duration.TimeSpan.TotalSeconds : 0d,
+            _ => (object)0d,
+        };
+    }
+
+    /// <inheritdoc />
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double == false)
+        {
+            return 0d;
+        }
+
+        var result = TimeSpan.FromTicks(System.Convert.ToInt64(TimeSpan.TicksPerSecond * (double)value));
+
+        // Do the conversion from visibility to bool
+        if (targetType == typeof(TimeSpan))
+        {
+            return result;
+        }
+
+        return targetType == typeof(Duration)
+            ? new Duration(result)
+            : Activator.CreateInstance(targetType);
     }
 }
