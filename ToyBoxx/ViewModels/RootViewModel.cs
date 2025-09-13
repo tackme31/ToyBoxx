e430420be9a4bfe42db05294db5b28a1355573d0
Takumi Yamada <xirtardauq@gmail.com>
2025-09-13 18:43:57 +0900

    Show control bar

--- ToyBoxx/ViewModels/RootViewModel.cs
+++ ToyBoxx/ViewModels/RootViewModel.cs
@@ -6,12 +6,24 @@ namespace ToyBoxx.ViewModels;
 
 public class RootViewModel : ViewModelBase
 {
-
     internal void OnApplicationLoaded()
     {
+        if (IsApplicationLoaded)
+        {
+            return;
+        }
+
+        IsApplicationLoaded = true;
     }
 
     public AppCommands Commands { get; } = new AppCommands();
 
     public Lazy<MediaElement> MediaElement => new(() => (Application.Current.MainWindow as MainWindow)?.Media ?? throw new Exception("Media element does not found."));
+
+    private bool _isApplicationLoaded;
+    public bool IsApplicationLoaded
+    {
+        get => _isApplicationLoaded;
+        set => SetProperty(ref _isApplicationLoaded, value);
+    }
 }
