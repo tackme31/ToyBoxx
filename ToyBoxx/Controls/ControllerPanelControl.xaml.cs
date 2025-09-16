405c24ea1e8ebdf86b289b5cb9878283fd8de4e0
Takumi Yamada <xirtardauq@gmail.com>
2025-09-16 23:39:55 +0900

    Add HtmlLikeSlider

--- ToyBoxx/Controls/ControllerPanelControl.xaml.cs
+++ ToyBoxx/Controls/ControllerPanelControl.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿using System.Windows.Controls;
 using Unosquare.FFME.Common;
 
 namespace ToyBoxx.Controls
@@ -12,89 +8,9 @@ namespace ToyBoxx.Controls
     /// </summary>
     public partial class ControllerPanelControl : UserControl
     {
-        private MediaPlaybackState _mediaStateBeforeDraggingStart;
-        private bool _isPositionSliderDragging = false;
-
         public ControllerPanelControl()
         {
             InitializeComponent();
-        }
-
-        private void PositionSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            // Custom Slider behavior:
-            // - Click on the track moves the thumb immediately to the clicked position
-            // - The thumb can be dragged immediately after clicking without releasing the mouse
-            // - Slider value updates in real-time while dragging
-            // - Mouse release finalizes the value
-
-            var slider = (Slider)sender;
-            if (!slider.IsEnabled)
-            {
-                return;
-            }
-
-            if (VisualTreeHelper.GetChild(slider, 0) is FrameworkElement root &&
-                root.FindName("PART_Track") is Track track)
-            {
-                // Save media state before dragging
-                _mediaStateBeforeDraggingStart = App.ViewModel.MediaElement.MediaState;
-
-                // Pause when dragging
-                App.ViewModel.Commands.Pause.Execute(null);
-
-                // Convert mouse position to Track's coordinate system
-                Point posOnTrack = e.GetPosition(track);
-
-                // Immediately set Value to the clicked position
-                slider.Value = track.ValueFromPoint(posOnTrack);
-
-                // Force layout update to sync Thumb's visual position
-                slider.UpdateLayout();
-
-                // Start custom dragging (without relying on Thumb's internal drag)
-                _isPositionSliderDragging = true;
-                slider.CaptureMouse();
-
-                // Start dragging
-                e.Handled = true;
-            }
-        }
-
-        private void PositionSlider_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (!_isPositionSliderDragging)
-            {
-                return;
-            }
-
-            var slider = (Slider)sender;
-            var pos = e.GetPosition(slider);
-
-            if (VisualTreeHelper.GetChild(slider, 0) is FrameworkElement root &&
-                root.FindName("PART_Track") is Track track)
-            {
-                // Update Value based on current mouse position on Track
-                slider.Value = track.ValueFromPoint(pos);
-            }
-        }
-
-        private void PositionSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (!_isPositionSliderDragging)
-            {
-                return;
-            }
-
-            var slider = (Slider)sender;
-            _isPositionSliderDragging = false;
-            slider.ReleaseMouseCapture();
-
-            if (_mediaStateBeforeDraggingStart == MediaPlaybackState.Manual ||
-                _mediaStateBeforeDraggingStart == MediaPlaybackState.Play)
-            {
-                App.ViewModel.Commands.Play.Execute(null);
-            }
         }
     }
 }
---
+++ ToyBoxx/Controls/HtmlLikeSlider.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ToyBoxx.Controls;
+
+public class HtmlLikeSlider : Slider
+{
+    public event EventHandler? DragStarted;
+    public event EventHandler? DragCompleted;
+
+    private bool _isDraggingThumb = false;
+
+    protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnPreviewMouseLeftButtonDown(e);
+
+        var pos = e.GetPosition(this);
+        double relativePos = Orientation == Orientation.Horizontal
+            ? pos.X / ActualWidth
+            : 1.0 - (pos.Y / ActualHeight); // 縦方向は逆
+
+        Value = Minimum + (Maximum - Minimum) * relativePos;
+
+        _isDraggingThumb = true;
+        CaptureMouse();
+        e.Handled = true;
+
+        DragStarted?.Invoke(this, EventArgs.Empty);
+    }
+
+    protected override void OnPreviewMouseMove(MouseEventArgs e)
+    {
+        base.OnPreviewMouseMove(e);
+
+        if (_isDraggingThumb && e.LeftButton == MouseButtonState.Pressed)
+        {
+            var pos = e.GetPosition(this);
+            double relativePos = Orientation == Orientation.Horizontal
+                ? pos.X / ActualWidth
+                : 1.0 - (pos.Y / ActualHeight);
+
+            Value = Minimum + (Maximum - Minimum) * relativePos;
+        }
+    }
+
+    protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+    {
+        base.OnPreviewMouseLeftButtonUp(e);
+
+        if (_isDraggingThumb)
+        {
+            _isDraggingThumb = false;
+            ReleaseMouseCapture();
+            e.Handled = true;
+
+            DragCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
